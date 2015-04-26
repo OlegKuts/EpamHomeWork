2adf98f33764a2435758561d07befacc036d3532
OlegKuts <kuts.oleg91@gmail.com>
2015-04-26 13:08:01 +0300

    Node class

--- EpamHW4/src/arraylinkedlist/ArrayLinkedList.java
+++ EpamHW4/src/arraylinkedlist/ArrayLinkedList.java
@@ -1,7 +1,26 @@
 package arraylinkedlist;
 
-public class ArrayLinkedList {
-private int listSize = 0;
+public class ArrayLinkedList<T> {
+	private int listSize = 0;
+	private int n = 0;
 
+	public ArrayLinkedList(int n) {
+		this.n = n;
+	}
+
+	private class Node<T> {
+		private T element;
+		private Node<T> next;
+		private Node<T> previous;
+		private T[] elementsArr;
+
+		public Node(T element, Node<T> next, Node<T> previous) {
+			this.element = element;
+			this.next = next;
+			this.previous = previous;
+			this.elementsArr = (T[]) new Object[n];
+		}
+
+	}
 
 }
